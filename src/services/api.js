f8c52626d30db4b4f5b277a74af084016ccb6165
mohamedroshansafeek <mohamedroshansafeeky@gmail.com>
2026-04-18 12:06:30 +0530

    backend URL updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://online-course-portal-b2pg.onrender.com',
+  baseURL: 'https://course-portal-hhlr.onrender.com',
 });
 
 api.interceptors.request.use(
